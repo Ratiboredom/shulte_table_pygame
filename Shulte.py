0bc232d9ed059446d902b4c0d82c37980ee5326a
Ratibor <ratibor.podlesny@gmail.com>
2024-07-03 00:08:44 +0400

    Completely redesigned the game. Fixed the bug of clickes not being registered. Added highlighting of clicked cells  for better experience. Added timer.

--- Shulte.py
+++ Shulte.py
@@ -1,139 +1,131 @@
-import pygame, random
+import pygame as pg
+import random
+import time
 
-pygame.init()
 
-screen = pygame.display.set_mode((800, 900))
-
-pygame.display.set_caption("Shulte table")
-
-clock = pygame.time.Clock()
+pg.init()
+SCREEN_SIZE = (800, 900)
+screen = pg.display.set_mode(SCREEN_SIZE)
+pg.display.set_caption("Shulte table")
 
 screen.fill((255, 255, 255))
+IMG_SIZE = 160
 
-number1 = pygame.image.load('1.jpg').convert()
-number10 = pygame.image.load('10.jpg').convert()
-number2 = pygame.image.load('2.jpg').convert()
-number11 = pygame.image.load('11.jpg').convert()
-number3 = pygame.image.load('3.jpg').convert()
-number12 = pygame.image.load('12.jpg').convert()
-number4 = pygame.image.load('4.jpg').convert()
-number13 = pygame.image.load('13.jpg').convert()
-number5 = pygame.image.load('5.jpg').convert()
-number14 = pygame.image.load('14.jpg').convert()
-number6 = pygame.image.load('6.jpg').convert()
-number15 = pygame.image.load('15.jpg').convert()
-number7 = pygame.image.load('7.jpg').convert()
-number16 = pygame.image.load('16.jpg').convert()
-number8 = pygame.image.load('8.jpg').convert()
-number17 = pygame.image.load('17.jpg').convert()
-number9 = pygame.image.load('9.jpg').convert()
-number18 = pygame.image.load('18.jpg').convert()
-number19 = pygame.image.load('19.jpg').convert()
-number20 = pygame.image.load('20.jpg').convert()
-number21 = pygame.image.load('21.jpg').convert()
-number22 = pygame.image.load('22.jpg').convert()
-number23 = pygame.image.load('23.jpg').convert()
-number24 = pygame.image.load('24.jpg').convert()
-number25 = pygame.image.load('25.jpg').convert()
+def get_img_pos(pos):
+        return (pos % 5 * IMG_SIZE, pos // 5 * IMG_SIZE)
 
-font = pygame.font.Font(None, 38)
-text1 = font.render("Next number:", False, [5, 5, 5])
+class Number:
+    """Each table cell has a num_value, an image, and position.
+    Positions are counted as following:
+    [0 1 2
+     3 4 5
+     6 7 8]
+     the default table is 5x5
+    """
 
-next_number = 1
+    def __init__(self, value:int, path_to_img:str, pos:int):
+        self.value = value
+        self.img = pg.image.load(path_to_img).convert()
+        self.pos = pos
+        self.img_pos = get_img_pos(pos)
+    
 
-numbers = [number1, number2, number3, number4, number5, number6, number7, number8, number9,
-           number10, number11, number12, number13, number14, number15, number16, number17, number18,
-           number19, number20, number21, number22, number23, number24, number25]
+numbers = []
+positions = [i for i in range(25)]
+random.shuffle(positions)
+table = {} # {position: value} (to instantly find a number by position when table is clicked)
+for i in range(1, 26):
+    pos = positions.pop()
+    numbers.append(Number(value=i, path_to_img=f"./img/{i}.jpg", pos=pos))
+    table[pos] = i
 
-positions = [(0, 0), (160, 0), (320, 0), (480, 0), (640, 0), (0, 160), (160, 160), (320, 160), (480, 160), (640, 160),
-             (0, 320),
-             (160, 320), (320, 320), (480, 320), (640, 320), (0, 480), (160, 480), (320, 480), (480, 480), (640, 480),
-             (0, 640),
-             (160, 640), (320, 640), (480, 640), (640, 640)]
+for num in numbers:
+    screen.blit(num.img, num.img_pos)
 
-pos1 = positions.pop(random.randrange(0, 24 + 1))
-pos2 = positions.pop(random.randrange(0, 23 + 1))
-pos3 = positions.pop(random.randrange(0, 22 + 1))
-pos4 = positions.pop(random.randrange(0, 21 + 1))
-pos5 = positions.pop(random.randrange(0, 20 + 1))
-pos6 = positions.pop(random.randrange(0, 19 + 1))
-pos7 = positions.pop(random.randrange(0, 18 + 1))
-pos8 = positions.pop(random.randrange(0, 17 + 1))
-pos9 = positions.pop(random.randrange(0, 16 + 1))
-pos10 = positions.pop(random.randrange(0, 15 + 1))
-pos11 = positions.pop(random.randrange(0, 14 + 1))
-pos12 = positions.pop(random.randrange(0, 13 + 1))
-pos13 = positions.pop(random.randrange(0, 12 + 1))
-pos14 = positions.pop(random.randrange(0, 11 + 1))
-pos15 = positions.pop(random.randrange(0, 10 + 1))
-pos16 = positions.pop(random.randrange(0, 9 + 1))
-pos17 = positions.pop(random.randrange(0, 8 + 1))
-pos18 = positions.pop(random.randrange(0, 7 + 1))
-pos19 = positions.pop(random.randrange(0, 6 + 1))
-pos20 = positions.pop(random.randrange(0, 5 + 1))
-pos21 = positions.pop(random.randrange(0, 4 + 1))
-pos22 = positions.pop(random.randrange(0, 3 + 1))
-pos23 = positions.pop(random.randrange(0, 2 + 1))
-pos24 = positions.pop(random.randrange(0, 1 + 1))
-pos25 = (random.choice(positions))
+font_small = pg.font.SysFont('Arial', 36)
+font_big = pg.font.SysFont('Arial', 48)
+next_num = 1
+text = font_small.render("Next number:", True, [5, 5, 5])
+text_pos = (80, 830)
+screen.blit(text, text_pos)
+next_num_text= font_big.render(str(next_num), True, [5, 5, 5])
+next_num_text_pos = (270, 820)
+screen.blit(next_num_text, next_num_text_pos)
 
-pozs = [pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9,
-        pos10, pos11, pos12, pos13, pos14, pos15, pos16, pos17,
-        pos18, pos19, pos20, pos21, pos22, pos23, pos24, pos25]
+# Transparent surface for highlighting clicked cells
+surface = pg.Surface((IMG_SIZE, IMG_SIZE), pg.SRCALPHA)
+surface.set_alpha(60)
+
+def highlight_cell(pos_clicked:tuple[int, int], color:str, clicked_num:int):
+    if color == 'green':
+        color = pg.Color(50, 255, 50)
+    elif color == 'red':
+        color = pg.Color(255, 50, 50)
+    img_x, img_y = get_img_pos(pos_clicked)
+    square = pg.Rect(0, 0, IMG_SIZE, IMG_SIZE)
+    pg.draw.rect(surface, color, rect=square)
+    screen.blit(numbers[clicked_num-1].img, numbers[clicked_num-1].img_pos)
+    screen.blit(surface, (img_x, img_y))
+
+mouse_down = False
+cell_highlighted = False
+correct_num = False
 
 run = True
+start_time = time.time()
 
 while run:
-    pygame.time.delay(100)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             run = False
 
-    screen.blit(number1, pos1)
-    screen.blit(number2, pos2)
-    screen.blit(number3, pos3)
-    screen.blit(number4, pos4)
-    screen.blit(number5, pos5)
-    screen.blit(number6, pos6)
-    screen.blit(number7, pos7)
-    screen.blit(number8, pos8)
-    screen.blit(number9, pos9)
-    screen.blit(number10, pos10)
-    screen.blit(number11, pos11)
-    screen.blit(number12, pos12)
-    screen.blit(number13, pos13)
-    screen.blit(number14, pos14)
-    screen.blit(number15, pos15)
-    screen.blit(number16, pos16)
-    screen.blit(number17, pos17)
-    screen.blit(number18, pos18)
-    screen.blit(number19, pos19)
-    screen.blit(number20, pos20)
-    screen.blit(number21, pos21)
-    screen.blit(number22, pos22)
-    screen.blit(number23, pos23)
-    screen.blit(number24, pos24)
-    screen.blit(number25, pos25)
+    if pg.mouse.get_pressed()[0] and not mouse_down:
+        mx, my = pg.mouse.get_pos()
+        if mx < 5 * IMG_SIZE and my < 5 * IMG_SIZE: # check if the click is within the table
+            pos_clicked = (my // IMG_SIZE * 5 + mx // IMG_SIZE)
+            clicked_num = table[pos_clicked]
+            if next_num == clicked_num:
+                correct_num = True
+                if not cell_highlighted:
+                    highlight_cell(pos_clicked, 'green', clicked_num)
+                    cell_highlighted = True
+            else:
+                correct_num = False
+                if not cell_highlighted:
+                    highlight_cell(pos_clicked, 'red', clicked_num)
+                    cell_highlighted = True
+            pg.display.flip()
+        mouse_down = True
 
-    screen.blit(text1, (270, 840))
-    text2 = font.render(str(next_number), False, [5, 5, 5])
-    screen.blit(text2, (445, 840))
+    if mouse_down and not pg.mouse.get_pressed()[0]: # if mouse was clicked and now is released
+        # removing cell highlighting
+        if cell_highlighted:
+            screen.blit(numbers[clicked_num-1].img, numbers[clicked_num-1].img_pos)
+            cell_highlighted = False
+        if correct_num:
+            next_num += 1
+            # painting over the previous number with white rectangle
+            pg.draw.rect(screen, (255, 255, 255), (next_num_text_pos, (60, 60)))
+            text = font_big.render(str(next_num), True, [5, 5, 5])
+            screen.blit(text, next_num_text_pos)
+            correct_num = False
+        mouse_down = False
+            
+    #Displaying timer:
+    elapsed_time = time.time() - start_time
+    minutes = int(elapsed_time // 60)
+    seconds = int(elapsed_time % 60)
+    milliseconds = int((elapsed_time * 1000) % 10)
+    elapsed_time_str = f"{minutes:1}:{seconds:02}:{milliseconds:01}"
 
-    if event.type == pygame.MOUSEBUTTONDOWN:
-        mx, my = pygame.mouse.get_pos()
-        if mx > pozs[next_number - 1][0] \
-        and mx < pozs[next_number - 1][0] + 161 \
-        and my > pozs[next_number - 1][1] \
-        and my < pozs[next_number - 1][1] + 161:
-            next_number += 1
-            print(next_number)
-            pygame.draw.rect(screen, (255, 255, 255), (440, 840, 50, 25))
-            text2 = font.render(str(next_number), False, [5, 5, 5])
-            screen.blit(text2, (445, 840))
+    timer_text = font_small.render(elapsed_time_str, True, [5, 5, 5])
+    timer_pos = (680, 830)
+    pg.draw.rect(screen, (255, 255, 255), ((600, 800), (200, 100)))
+    screen.blit(timer_text, timer_pos)
+    pg.display.flip()
 
-    if next_number == 26:
+    if next_num == 26:
         run = False
+    pg.display.update()
 
-    # pygame.display.flip()
-    pygame.display.update()
+print(f"Your time is {elapsed_time_str}")
